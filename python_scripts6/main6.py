35afaa8f8a8609bc2f9401fce71636bcb55334d9
Pavelwell7 <pavelbugaev303@gmail.com>
2025-07-30 19:57:59 +0300

    Update main6.py

--- python_scripts6/main6.py
+++ python_scripts6/main6.py
@@ -1,21 +1,44 @@
+def has_digit(password):
+    return any(letter.isdigit() for letter in password)
+
+
+def is_very_long(password):
+    password_length = len(password)
+    return password_length > 12
+
+
+def has_upper_letters(password):
+    return any(letter.isupper() for letter in password)
+
+
+def has_lower_letters(password):
+    return any(letter.islower() for letter in password)
+
+
+def has_symbols(password):
+    return any(not letter.isalnum() for letter in password)
+
+
 def calculate_password_score(password):
     score = 0
-    password_length = len(password)
-    if any(letter.isdigit() for letter in password):
-        score += 2
-    if password_length > 12:
-        score += 2
-    if any(letter.islower() for letter in password):
-        score += 2
-    if any(letter.isupper() for letter in password):
-        score += 2
-    if any(not letter.isalnum() for letter in password):
-        score += 2
+    secondary_functions = [
+        has_digit,
+        is_very_long,
+        has_upper_letters,
+        has_lower_letters,
+        has_symbols,
+    ]
+    for second in secondary_functions:
+        if second(password):
+            score += 2
     return score
 
 
-if __name__ == '__main__':
+def main():
     password = input('Введите пароль: ')
     score = calculate_password_score(password)
     print(f'Рейтинг пароля: {score}')
-    
+
+
+if __name__ == '__main__':
+    main()
